80c7ae95d7f8f63f4fc4ee659451046a7ff3ab40
Delta11235 <mateusz._01@wp.pl>
2023-09-19 21:57:59 +0200

    ok

---
+++ script.js
@@ -0,0 +1,7 @@
+var container = document.getElementById("container");
+var button = document.getElementById("button");
+var input = document.getElementById("input");
+
+button.addEventListener("click") = () => {
+    var newDiv = container.createElement("div")
+}
