1dbf6d554b7ce0bcd5f12133150d65c1a21417ef
Delta11235 <mateusz._01@wp.pl>
2023-09-19 23:02:55 +0200

    wysylanie wiadomosci dziala

--- script.js
+++ script.js
@@ -2,13 +2,15 @@ var container = document.getElementById("container");
 var button = document.getElementById("button");
 var input = document.getElementById("input");
 
-button.addEventListener("click") = () => {
-    var newDiv = container.createElement("div")
-    newDiv.style.width = "60"+"%";
-    newDiv.style.height = "30"+"%";
-    newDiv.style.backgroundColor = "blue"
-    var newText = newDiv.createElement("p")
-    newText.value = "ok"
-    document.container.appendChild(newDiv);
-    document.newDiv.appendChild(newText);
-}
+button.addEventListener("click", () => {
+  var newDiv = document.createElement("div");
+  newDiv.style.width = "30" + "%";
+  newDiv.style.height = "10" + "%";
+  newDiv.style.borderRadius = "20" + "%";
+  newDiv.style.backgroundColor = "blue";
+  var newText = document.createElement("p");
+  newText.innerText = input.value;
+  container.appendChild(newDiv);
+  newDiv.appendChild(newText);
+  input.value = "";
+});
